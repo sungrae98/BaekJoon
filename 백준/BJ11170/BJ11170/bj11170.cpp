1a8e2e7227981a2eccfe7034667f6f46c29a784e
shtjdfo-15 <shtjdfo15@gmail.com>
2023-07-19 08:53:01 +0900

    23.07.19

--- 백준/BJ11170/BJ11170/bj11170.cpp
+++ 백준/BJ11170/BJ11170/bj11170.cpp
@@ -10,7 +10,21 @@ int main() {
 
 		cin >> a >> b;
 		for (int j = a; j <= b; j++) {
+			if (j == 0) {
+				result++;
+			}
 
+			int tmp = j;
+			// 10으로 나눴을 때 나머지가 0이면 0을 가지고 있다는 것이다.
+			while (tmp > 0)
+			{
+				if (tmp % 10 == 0) {
+					result++;
+				}
+				tmp /= 10;
+			}
 		}
+		cout << result << '\n';
 	}
+	return 0;
 }
